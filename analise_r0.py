c8f9bd4c48d59cc00b0535854efe262ddffe2ed4
Guido <guidoneulaender@gmail.com>
2020-07-13 21:02:49 -0300

    Ta pronto o calculo de r0... Ula ula

--- analise_r0.py
+++ analise_r0.py
@@ -3,15 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from constantes import omega, N
 
+def smoother(C,n):
+    Cs = C[2*n:]
+    for i in range(-n+1,n+1):
+        Cs += C[n-i:-(n+i)]
+    return Cs/(2*n+1)
+
 input_file = 'data/BR.csv'
 C = np.genfromtxt(input_file, delimiter=',', usecols = (1))
-Cs = C
+Cs = smoother(C,2)
 dC = Cs[1:] - Cs[:-1]
 d2C = dC[1:] - dC[:-1]
+
+Cs = Cs[2:]
+dC = dC[1:]
 
 alpha_max = 20
 alpha_min = 10
 
-mi = lambda alpha, dC, d2C: (alpha/(omega*N)) * (omega*C + dC)
+mi = lambda alpha, C, dC: (alpha/(omega*N)) * (omega*C + dC)
 
-r0 = lambda alpha, C, dC, d2C: (1/(1-mi(alpha,dC,d2C)) + (d2C/( omega*dC*(1-mi(alpha,dC,d2C)) ))
+r0 = lambda alpha, C, dC, d2C: (1/(1-mi(alpha,C,dC))) + (d2C/( omega*dC*(1-mi(alpha,C,dC)) ))
+
+r0_max = r0(alpha_max, Cs, dC, d2C)
+r0_min = r0(alpha_min, Cs, dC, d2C)
+r0_aprox = (r0_max + r0_min)/2
