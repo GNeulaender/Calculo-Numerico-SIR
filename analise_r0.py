f17c5ff790079c2c9fd7903660a44158ad463041
Espionild0 <67872004+Espionild0@users.noreply.github.com>
2020-07-13 21:36:31 -0300

    usei um código para colocar os dados de r0 em um arquivo csv

--- analise_r0.py
+++ analise_r0.py
@@ -11,7 +11,7 @@ def smoother(C,n):
 
 input_file = 'data/BR.csv'
 C = np.genfromtxt(input_file, delimiter=',', usecols = (1))
-Cs = smoother(C,2)
+Cs = smoother(C,4)
 dC = Cs[1:] - Cs[:-1]
 d2C = dC[1:] - dC[:-1]
 
@@ -28,3 +28,14 @@ r0 = lambda alpha, C, dC, d2C: (1/(1-mi(alpha,C,dC))) + (d2C/( omega*dC*(1-mi(al
 r0_max = r0(alpha_max, Cs, dC, d2C)
 r0_min = r0(alpha_min, Cs, dC, d2C)
 r0_aprox = (r0_max + r0_min)/2
+
+
+#criando um arquivo .csv com o r0
+
+with open('r0dados.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["r0 máximo", "r0 mínimo", "r0 aproximado"])
+    for i in range(0,len(r0_max)):
+        row = [r0_max[i],r0_min[i],r0_aprox[i]]
+
+        writer.writerow(row)
